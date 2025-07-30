82eae174c1870941007ba6e0c04654db76bc89d7
Mayank <bhatasanamayank06@gmail.com>
2025-07-30 19:26:47 +0530

    sumAll.js complete

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    max = Math.max(num1, num2);
+    min = Math.min(num1, num2);
+    let sum = 0;
+    for(let i = min; i <= max; i++) sum+=i
+    return sum;
 };
 
 // Do not edit below this line
