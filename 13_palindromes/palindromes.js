ab75bb8887d61fb90844649ceef9d998f39aa60b
Mayank <bhatasanamayank06@gmail.com>
2025-07-30 20:37:47 +0530

    palindromes.js complete

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    // With map
+
+    // const map = new Map()
+    // map.set(' ', true);
+    // map.set(',', true);
+    // map.set('!', true);
+    // map.set('.', true);
+
+
+    // With Object
+    const obg = {
+        ' ' : true,
+        '.' : true,
+        '!' : true,
+        ',' : true,
+    }
+
+    // for map use !map.has(val)
+    const temp = string.split('').filter(val => !obg[val]);
+    const temp2 = temp.join('').toLowerCase();
+    const temp3 = temp2.split('').reverse().join('')
+    return temp2 === temp3
 };
 
 // Do not edit below this line
