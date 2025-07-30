d941ac290d5f3a22fd75fad379f805c3d81616f1
Mayank <bhatasanamayank06@gmail.com>
2025-07-30 17:35:28 +0530

    repeatString.js complete

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, nums) {
+    let temp = '';
+    if(nums < 0 ) return "ERROR"
+    for (let i = 0; i < nums; i++)
+        temp += string;
 
+    return temp;
 };
 
 // Do not edit below this line
