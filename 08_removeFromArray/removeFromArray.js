ad07f69b0852b8a9c376fb105173a56107b907a9
Mayank <bhatasanamayank06@gmail.com>
2025-07-30 19:09:47 +0530

    removeFromArray.js complete

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(...arr) {
+    const map = new Map()
+    for (let i = 1; i < arr.length; i++) map.set(arr[i], true);
+    return arr[0].filter(item => !map.has(item))
 };
 
 // Do not edit below this line
