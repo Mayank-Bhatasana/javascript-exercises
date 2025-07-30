e44b9889b1827f30ec03c5b42ae89bbf40592f71
Mayank <bhatasanamayank06@gmail.com>
2025-07-30 17:40:03 +0530

    reverseString.js complete

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let temp = '';
 
+    for (let i = string.length - 1; i >= 0; i--)
+        temp += string.charAt(i);
+
+    return temp;
 };
 
 // Do not edit below this line
